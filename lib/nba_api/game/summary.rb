bb2daeae7e097c8a961ba67b4e1ba50dcdb424e7
4ndeez <andissteam@gmail.com>
2024-12-12 23:40:36 +0300

    add game/summary endpoint

---
+++ lib/nba_api/game/summary.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module NbaApi
+  module Game
+    module Summary
+      extend Resource
+      extend Params
+
+      ENDPOINT = "boxscoresummaryv3"
+
+      class << self
+        def call(options = {})
+          params = build_params(options)
+          get(ENDPOINT, params)
+        end
+      end
+    end
+  end
+end
