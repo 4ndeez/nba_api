fa0c835f740ac19740ea646f977a52fb8b648909
4ndeez <andissteam@gmail.com>
2025-01-25 23:54:40 +0300

    modify error class

--- lib/nba_api/errors/invalid_parameter_error.rb
+++ lib/nba_api/errors/invalid_parameter_error.rb
@@ -1,5 +1,15 @@
 module NbaApi
   module Errors
-    class InvalidParameterError < StandardError; end
+    class InvalidParameterError < StandardError
+      def initialize(parameter_name, valid_options = nil)
+        message = if valid_options && valid_options.any?
+          "Invalid #{parameter_name}. Valid options: #{valid_options.join(", ")}"
+        else
+          "Invalid #{parameter_name}"
+        end
+        
+        super(message)
+      end
+    end
   end
 end
