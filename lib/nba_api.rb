bd069e0f5af20049ef7c2d3052d377642963f19d
4ndeez <andissteam@gmail.com>
2025-02-02 23:16:40 +0300

    add required files

--- lib/nba_api.rb
+++ lib/nba_api.rb
@@ -9,6 +9,7 @@ require_relative "nba_api/resource"
 
 # Errors
 require_relative "nba_api/errors/invalid_parameter_error"
+require_relative "nba_api/errors/missing_parameter_error"
 
 # League API
 require_relative "nba_api/league/params"
@@ -39,6 +40,7 @@ require_relative "nba_api/game/play_by_play_win_probability"
 require_relative "nba_api/team/params"
 require_relative "nba_api/team/details"
 require_relative "nba_api/team/gamelogs"
+require_relative "nba_api/team/history"
 require_relative "nba_api/team/leaders"
 require_relative "nba_api/team/roster"
 
@@ -46,17 +48,107 @@ require_relative "nba_api/team/roster"
 require_relative "nba_api/player/params"
 require_relative "nba_api/player/info"
 
-# Stats/Teams API
-require_relative "nba_api/stats/teams/params"
-require_relative "nba_api/stats/teams/advanced"
-require_relative "nba_api/stats/teams/defense"
-require_relative "nba_api/stats/teams/four_factors"
-require_relative "nba_api/stats/teams/opponent"
-require_relative "nba_api/stats/teams/misc"
-require_relative "nba_api/stats/teams/scoring"
-require_relative "nba_api/stats/teams/traditional"
-require_relative "nba_api/stats/teams/violations"
-require_relative "nba_api/stats/teams/history"
+# Stats/Lineups API
+require_relative "nba_api/stats/lineups/params"
+require_relative "nba_api/stats/lineups/advanced"
+require_relative "nba_api/stats/lineups/four_factors"
+require_relative "nba_api/stats/lineups/misc"
+require_relative "nba_api/stats/lineups/opponent"
+require_relative "nba_api/stats/lineups/scoring"
+require_relative "nba_api/stats/lineups/traditional"
+
+# Stats/Teams/Clutch API
+require_relative "nba_api/stats/teams/clutch/params"
+require_relative "nba_api/stats/teams/clutch/advanced"
+require_relative "nba_api/stats/teams/clutch/misc"
+require_relative "nba_api/stats/teams/clutch/scoring"
+require_relative "nba_api/stats/teams/clutch/traditional"
+require_relative "nba_api/stats/teams/clutch/usage"
+
+# Stats/Teams/Defense API
+require_relative "nba_api/stats/teams/defense/params"
+require_relative "nba_api/stats/teams/defense/overall"
+require_relative "nba_api/stats/teams/defense/three_pointers"
+require_relative "nba_api/stats/teams/defense/two_pointers"
+require_relative "nba_api/stats/teams/defense/under_fifteen_foot"
+require_relative "nba_api/stats/teams/defense/under_six_foot"
+require_relative "nba_api/stats/teams/defense/under_ten_foot"
+
+# Stats/Teams/General API
+require_relative "nba_api/stats/teams/general/params"
+require_relative "nba_api/stats/teams/general/advanced"
+require_relative "nba_api/stats/teams/general/defense"
+require_relative "nba_api/stats/teams/general/four_factors"
+require_relative "nba_api/stats/teams/general/opponent"
+require_relative "nba_api/stats/teams/general/misc"
+require_relative "nba_api/stats/teams/general/scoring"
+require_relative "nba_api/stats/teams/general/traditional"
+require_relative "nba_api/stats/teams/general/violations"
+
+# Stats/Teams/Playtypes API
+require_relative "nba_api/stats/teams/playtypes/params"
+require_relative "nba_api/stats/teams/playtypes/cut"
+require_relative "nba_api/stats/teams/playtypes/handoff"
+require_relative "nba_api/stats/teams/playtypes/isolation"
+require_relative "nba_api/stats/teams/playtypes/misc"
+require_relative "nba_api/stats/teams/playtypes/off_screen"
+require_relative "nba_api/stats/teams/playtypes/pick_roll_ball_handler"
+require_relative "nba_api/stats/teams/playtypes/pick_roll_roll_man"
+require_relative "nba_api/stats/teams/playtypes/post_up"
+require_relative "nba_api/stats/teams/playtypes/putback"
+require_relative "nba_api/stats/teams/playtypes/spot_up"
+require_relative "nba_api/stats/teams/playtypes/transition"
+
+# Stats/Players/Shooting API
+require_relative "nba_api/stats/teams/shooting/params"
+require_relative "nba_api/stats/teams/shooting/by_zone"
+require_relative "nba_api/stats/teams/shooting/catch_shoot"
+require_relative "nba_api/stats/teams/shooting/closest_defender"
+require_relative "nba_api/stats/teams/shooting/closest_defender_plus_ten"
+require_relative "nba_api/stats/teams/shooting/dribble"
+require_relative "nba_api/stats/teams/shooting/eight_foot_range"
+require_relative "nba_api/stats/teams/shooting/five_foot_range"
+require_relative "nba_api/stats/teams/shooting/general"
+require_relative "nba_api/stats/teams/shooting/opponent_by_zone"
+require_relative "nba_api/stats/teams/shooting/opponent_eight_foot_range"
+require_relative "nba_api/stats/teams/shooting/opponent_five_foot_range"
+require_relative "nba_api/stats/teams/shooting/pull_up"
+require_relative "nba_api/stats/teams/shooting/shotclock"
+require_relative "nba_api/stats/teams/shooting/touch_time"
+require_relative "nba_api/stats/teams/shooting/under_ten_foot"
+
+# Stats/Teams/Tracking API
+require_relative "nba_api/stats/teams/tracking/params"
+require_relative "nba_api/stats/teams/tracking/catch_shoot"
+require_relative "nba_api/stats/teams/tracking/defensive_impact"
+require_relative "nba_api/stats/teams/tracking/drives"
+require_relative "nba_api/stats/teams/tracking/elbow_touches"
+require_relative "nba_api/stats/teams/tracking/hustle"
+require_relative "nba_api/stats/teams/tracking/paint_touches"
+require_relative "nba_api/stats/teams/tracking/passing"
+require_relative "nba_api/stats/teams/tracking/possessions"
+require_relative "nba_api/stats/teams/tracking/post_touches"
+require_relative "nba_api/stats/teams/tracking/pull_up_shots"
+require_relative "nba_api/stats/teams/tracking/rebounding"
+require_relative "nba_api/stats/teams/tracking/shooting_efficiency"
+require_relative "nba_api/stats/teams/tracking/speed_distance"
+
+# Stats/Players/Clutch API
+require_relative "nba_api/stats/players/clutch/params"
+require_relative "nba_api/stats/players/clutch/advanced"
+require_relative "nba_api/stats/players/clutch/misc"
+require_relative "nba_api/stats/players/clutch/scoring"
+require_relative "nba_api/stats/players/clutch/traditional"
+require_relative "nba_api/stats/players/clutch/usage"
+
+# Stats/Players/Defense API
+require_relative "nba_api/stats/players/defense/params"
+require_relative "nba_api/stats/players/defense/overall"
+require_relative "nba_api/stats/players/defense/three_pointers"
+require_relative "nba_api/stats/players/defense/two_pointers"
+require_relative "nba_api/stats/players/defense/under_fifteen_foot"
+require_relative "nba_api/stats/players/defense/under_six_foot"
+require_relative "nba_api/stats/players/defense/under_ten_foot"
 
 # Stats/Players/General API
 require_relative "nba_api/stats/players/general/params"
@@ -68,14 +160,6 @@ require_relative "nba_api/stats/players/general/scoring"
 require_relative "nba_api/stats/players/general/traditional"
 require_relative "nba_api/stats/players/general/usage"
 require_relative "nba_api/stats/players/general/violations"
-
-# Stats/Players/Clutch API
-require_relative "nba_api/stats/players/clutch/params"
-require_relative "nba_api/stats/players/clutch/advanced"
-require_relative "nba_api/stats/players/clutch/misc"
-require_relative "nba_api/stats/players/clutch/scoring"
-require_relative "nba_api/stats/players/clutch/traditional"
-require_relative "nba_api/stats/players/clutch/usage"
 
 # Stats/Players/Playtypes API
 require_relative "nba_api/stats/players/playtypes/params"
@@ -91,35 +175,39 @@ require_relative "nba_api/stats/players/playtypes/putback"
 require_relative "nba_api/stats/players/playtypes/spot_up"
 require_relative "nba_api/stats/players/playtypes/transition"
 
+# Stats/Players/Shooting API
+require_relative "nba_api/stats/players/shooting/params"
+require_relative "nba_api/stats/players/shooting/by_zone"
+require_relative "nba_api/stats/players/shooting/catch_shoot"
+require_relative "nba_api/stats/players/shooting/closest_defender"
+require_relative "nba_api/stats/players/shooting/closest_defender_plus_ten"
+require_relative "nba_api/stats/players/shooting/dribble"
+require_relative "nba_api/stats/players/shooting/eight_foot_range"
+require_relative "nba_api/stats/players/shooting/five_foot_range"
+require_relative "nba_api/stats/players/shooting/general"
+require_relative "nba_api/stats/players/shooting/opponent_by_zone"
+require_relative "nba_api/stats/players/shooting/opponent_eight_foot_range"
+require_relative "nba_api/stats/players/shooting/opponent_five_foot_range"
+require_relative "nba_api/stats/players/shooting/pull_up"
+require_relative "nba_api/stats/players/shooting/shotclock"
+require_relative "nba_api/stats/players/shooting/touch_time"
+require_relative "nba_api/stats/players/shooting/under_ten_foot"
+
 # Stats/Players/Tracking API
 require_relative "nba_api/stats/players/tracking/params"
 require_relative "nba_api/stats/players/tracking/catch_shoot"
+require_relative "nba_api/stats/players/tracking/defensive_impact"
 require_relative "nba_api/stats/players/tracking/drives"
 require_relative "nba_api/stats/players/tracking/elbow_touches"
+require_relative "nba_api/stats/players/tracking/hustle"
 require_relative "nba_api/stats/players/tracking/paint_touches"
 require_relative "nba_api/stats/players/tracking/passing"
 require_relative "nba_api/stats/players/tracking/possessions"
 require_relative "nba_api/stats/players/tracking/post_touches"
 require_relative "nba_api/stats/players/tracking/pull_up_shots"
 require_relative "nba_api/stats/players/tracking/rebounding"
+require_relative "nba_api/stats/players/tracking/shooting_efficiency"
 require_relative "nba_api/stats/players/tracking/speed_distance"
-
-# Stats/Players/Defense API
-require_relative "nba_api/stats/players/defense/params"
-require_relative "nba_api/stats/players/defense/overall"
-require_relative "nba_api/stats/players/defense/three_pointers"
-require_relative "nba_api/stats/players/defense/two_pointers"
-require_relative "nba_api/stats/players/defense/under_fifteen_foot"
-require_relative "nba_api/stats/players/defense/under_six_foot"
-require_relative "nba_api/stats/players/defense/under_ten_foot"
-
-# Stats/Players/Shooting API
-require_relative "nba_api/stats/players/shooting/params"
-require_relative "nba_api/stats/players/shooting/catch_shoot"
-require_relative "nba_api/stats/players/shooting/general"
-require_relative "nba_api/stats/players/shooting/pull_up"
-require_relative "nba_api/stats/players/shooting/shotclock"
-require_relative "nba_api/stats/players/shooting/under_ten_foot"
 
 module NbaApi
   class Error < StandardError; end
